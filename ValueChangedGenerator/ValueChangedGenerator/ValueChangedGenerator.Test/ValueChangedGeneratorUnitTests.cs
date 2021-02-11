4b341ae7dc3004a623fdb20cc41a240c06aafc3b
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2021-02-11 22:06:24 +1000

    Remove tests

--- ValueChangedGenerator/ValueChangedGenerator/ValueChangedGenerator.Test/ValueChangedGeneratorUnitTests.cs
+++ ValueChangedGenerator/ValueChangedGenerator/ValueChangedGenerator.Test/ValueChangedGeneratorUnitTests.cs
@@ -1,64 +1,13 @@
 ﻿using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Xunit;
-using VerifyCS = ValueChangedGenerator.Test.CSharpCodeFixVerifier<
-    ValueChangedGenerator.ValueChangedGeneratorAnalyzer,
-    ValueChangedGenerator.ValueChangedGeneratorCodeFixProvider>;
 
 namespace ValueChangedGenerator.Test
 {
     public class ValueChangedGeneratorUnitTest
     {
-        //No diagnostics expected to show up
-        [Fact]
-        public async Task TestMethod1()
-        {
-            var test = @"";
-
-            await VerifyCS.VerifyAnalyzerAsync(test);
-        }
-
-        //Diagnostic and CodeFix both triggered and checked for
-        [Fact]
-        public async Task TestMethod2()
-        {
-            var test = @"
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using System.Diagnostics;
-
-    namespace ConsoleApplication1
-    {
-        class {|#0:TypeName|}
-        {   
-        }
-    }";
-
-            var fixtest = @"
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using System.Diagnostics;
-
-    namespace ConsoleApplication1
-    {
-        class TYPENAME
-        {   
-        }
-    }";
-
-            var expected = VerifyCS.Diagnostic("ValueChangedGenerator").WithLocation(0).WithArguments("TypeName");
-            await VerifyCS.VerifyCodeFixAsync(test, expected, fixtest);
-        }
-
         [Fact]
         public void GeneratePartialDeclaration()
         {
